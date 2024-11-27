3189bcafc1a21944cfa9d33ec742e6e69259683c
anpham <anpham16042005@gmail.com>
2024-11-27 10:34:16 +0700

    da them user

--- layout/index.php
+++ layout/index.php
@@ -1,12 +1,12 @@
 <?php
 include_once './Views/header.php';
 $page = isset($_GET['trang']) ? $_GET['trang'] : 'home';
-$id = isset($_GET['id']) ? $_GET['id'] :'';
-$iddb = isset($_GET['iddb']) ? $_GET['iddb'] :'';
-$iddm = isset($_GET['iddm']) ? $_GET['iddm'] :'';
+$id = isset($_GET['id']) ? $_GET['id'] : '';
+$iddb = isset($_GET['iddb']) ? $_GET['iddb'] : '';
+$iddm = isset($_GET['iddm']) ? $_GET['iddm'] : '';
 $chuyen = isset($_GET['chuyen']) ? $_GET['chuyen'] : '1';
 $kyw = isset($_GET['kyw']) ? $_GET['kyw'] : '';
-switch ($page) { 
+switch ($page) {
     case 'home':
         include_once 'Controllers/HomeController.php';
         $HomeController = new HomeController(id: $id, iddb: $iddb, iddm: $iddm);
@@ -17,7 +17,7 @@ switch ($page) {
         break;
     case 'allproduct':
         include_once 'Controllers/AllproductController.php';
-        $AllproductController = new AllproductController(id: $id,iddm: $iddm, chuyen :$chuyen, kyw: $kyw);
+        $AllproductController = new AllproductController(id: $id, iddm: $iddm, chuyen: $chuyen, kyw: $kyw);
         break;
     case 'cart':
         include_once 'Controllers/CartController.php';
@@ -38,6 +38,20 @@ switch ($page) {
     case 'productdetail':
         include_once 'Controllers/ProductdetailController.php';
         $ProductdetailController = new ProductdetailController(id_sp: $id);
+        break;
+    case 'user':
+        include_once 'Controllers/UserController.php';
+        $userController = new UserController();
+        $userController->infor();
+        break;
+
+    case 'address':
+        include_once 'Controllers/AddressController.php';
+        $AddressController = new AddressController();
+        break;
+    case 'history':
+        include_once 'Controllers/HistoryController.php';
+        $HistoryController = new HistoryController();
         break;
 }
 include_once './Views/footer.php';
