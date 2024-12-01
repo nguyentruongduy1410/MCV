1f9392bae6c3b8c3cbaa8c12873c155b8c6bcd60
Nguyen Truong Duy <nguyentruongduy1410@gmail.com>
2024-12-01 23:19:57 +0700

    chi tiet loi

--- layout/Controllers/ProductdetailController.php
+++ layout/Controllers/ProductdetailController.php
@@ -1,23 +1,10 @@
 <?php
 class ProductdetailController {
-    public function __construct($id_sp) {
+    public function __construct($id) {
         include_once 'Models/ProductdetailModel.php';
         $productdetailmodel = new ProductdetailModel();
-
-        // Kiểm tra tham số id_sp đã được nhận chưa
-        echo "Product ID: " . $id_sp; // Kiểm tra xem giá trị id có đúng không
-
-        // Lấy dữ liệu hình ảnh chi tiết cho sản phẩm theo id_sp
-        $productdetailmodel->chitiethinhanh($id_sp);
-
-        // Truyền dữ liệu từ Model sang View
-        $chitiethinhanh = $productdetailmodel->chitiethinhanh;
+        $productdetailmodel->chitiethinhanh($id);
         include_once './Views/productdetails.php';
     }
 }
-
-
-
-
-
 ?>
